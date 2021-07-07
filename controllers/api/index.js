93dc5004b5ea2b8dda7ea6ec8f36505c76fd1033
Irina Pyak <ir.pyak@gmail.com>
2021-07-06 20:42:42 -0400

    add linkRoutes.js, link.handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const linkRoutes = require('./linkRoutes');
 
 // end point is /api 
 router.use('/users', userRoutes);
+router.use('/links', linkRoutes);
 
 // export
 module.exports = router;
--- controllers/api/linkRoutes.js
+++ controllers/api/linkRoutes.js
@@ -2,25 +2,26 @@ const router = require("express").Router();
 const { Link, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.get('/cards', withAuth, async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     // Get all posts and JOIN with user data
     const linkData = await Link.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-      ],
+      // include: [
+      //   {
+      //     model: User,
+      //     attributes: ["username"]
+      //   },
+      // ],
     });
-
+    console.log("linkData:", linkData);
     // Serialize data so the template can read it
-    const posts = linkData.map((link) => link.get({ plain: true }));
-
+    const links = linkData.map((link) => link.get({ plain: true }));
+    console.log("links:", links);
     // Pass serialized data and session flag into template
-    res.render('link', { 
-      posts, 
-      logged_in: req.session.logged_in 
+    res.render("link", {
+      layout: "homepage",
+      links,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
