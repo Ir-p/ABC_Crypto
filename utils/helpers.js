d5f19f68821a8037d616d6415edabda905c532d4
Irina Pyak <ir.pyak@gmail.com>
2021-07-08 21:58:58 -0400

    edit link-view.handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -15,4 +15,15 @@ module.exports = {
       return `<span for="img" aria-label="gear">⚙️</span>`;
     }
   },
-};
+  switch: (value, options) => {
+
+    this.switch_value = value;
+    return options.fn(this);
+  },
+  
+ case: (value, options) => {
+    if (value == this.switch_value) {
+      return options.fn(this);
+    }
+  }
+}
