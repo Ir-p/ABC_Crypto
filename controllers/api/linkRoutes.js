6d74a3995bc802bdc0e799c19f6070b5b6491fc8
Nahid <Nahidmahmud14@yahoo.com>
2021-07-10 10:35:55 -0400

    css

--- controllers/api/linkRoutes.js
+++ controllers/api/linkRoutes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const { Link, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-<<<<<<< HEAD
 router.get("/", withAuth, async (req, res) => {
   try {
     // Get all posts and JOIN with user data
@@ -32,8 +31,6 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
-=======
->>>>>>> main
 router.post("/", withAuth, async (req, res) => {
   const body = req.body;
   // console.log("body:", body);
